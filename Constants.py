74f873ac1f0af7132a12c9ef7d1141dc4b734dd0
Sophie Redford <sophie.redford@gmail.com>
2015-03-18 08:49:41 +0100

    Added Constants file.
    Should be used for all constant parameters in the analysis,
    eg energies of sources, number of pixels etc.

---
+++ Constants.py
@@ -0,0 +1,11 @@
+# to define constants relevant to the calibration analysis
+
+# peak energies in keV
+FePeakE = 5.899
+Co1PeakE = 6.4
+CuPeakE = 8.1
+Co2PeakE = 14.4
+CdPeakE = 22.9
+InPeakE = 24.0
+Am2PeakE = 26.2
+Am3PeakE = 60.0
