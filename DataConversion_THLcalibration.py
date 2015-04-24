be002f73de63941404234d9ff831acf4f5429552
nalipour <nalipour@cern.ch>
2015-04-24 18:36:49 +0200

    THL calibration: DataConversion_THLcalibration.py reads the frames from eos and writes text files containing two columns with the THL and the number of counts. THLcalibration.py reads the text files generated by DataConversion_THLcalibration.py, calculates the derivative, performs a fitting and calculates the corresponding energy and error for a given THL.

--- DataConversion_THLcalibration.py
+++ DataConversion_THLcalibration.py
@@ -7,58 +7,22 @@ import math
 import os
 import numpy as np
 from scipy.cluster.hierarchy import fclusterdata
-
-
+from os import environ
+from itertools import product # SimpleClustering
 
 def SimpleClustering(matrix):
 
     frame=matrix
 
     for i,j in [[i,j] for i,j in product(xrange(256),xrange(256)) if matrix[i][j]!=0] :
-        for u,v in [[u,v] for u,v in product([-1,0,1],[-1,0,1]) if (((i+u>=0 and i+u<=255) and (j+v>=0 and j+v<=255)) and (u!=0 or v!=0)) ] :
-            if(matrix[i+u][j+v]!=0) :
+        for u,v in [[u,v] for u,v in product([-1,0,1],[-1,0,1]) if (((i+u>=0 and i+u<=255) and (j+v>=0 and j+v<=255)) and (u!=0 or v!=0))]:
+            if(matrix[i+u][j+v]!=0):
                 frame[i][j]=0
                 frame[i+u][j+v]=0
 
-    return frame
-
-# def readTextFile(filename, hist):
-#     print filename
-#     f = open(filename)
-#     lines = [line.strip() for line in open(filename)]
-    
-#     f.close()
-
-    
-#     for i in range(0, len(lines)):
-#         for k in range(0, len(lines[i].split())):
-#             hist.Fill(float(lines[i].split()[k]))
-
-# # def readInMatrix(filename):
-# #     f=open(filename , 'r')
-# #     matrix = [ map(float,line.split(' ')) for line in f ]
-# #     f.close()
-# #     return matrix
+    return sum(sum(frame, []))
 
 
-"""def derivative(x, y, color):
-
-    x_deriv=[]
-    deriv=[]
-    for i in range(0, len(x)-1):
-        x_deriv.append(x[i])
-        deriv.append((y[i+1]-y[i])/(x[i+1]-x[i]))
-
-    
-    g_deriv=TGraph(len(x_deriv), array('d', x_deriv), array('d', deriv))
-    g_deriv.SetMarkerStyle(20)
-    g_deriv.SetMarkerSize(0.8)
-
-    g_deriv.SetMarkerColor(color)
-    g_deriv.SetLineColor(color)
-
-    return g_deriv
-"""
 def writeFile(THL, Counts, OutputFile):
     txtfile = open(OutputFile, "w")
     
@@ -71,16 +35,6 @@ def writeFile(THL, Counts, OutputFile):
 def usage():
     print 'THL calibration'
     print 'Usage:\n python %s <assembly> <frames> <THLmin> <THLmax> <source>' % ( sys.argv[0] )
-# def erf(x, par):
-#     return par[0]*TMath.Erf(par[1]*x[0]+par[2])+par[3]
-
-# def fitHist(hist):
-
-#     maxBin=hist.GetMaximumBin()
-#     fit=TF1("GaussFit", "gaus", maxBin-1000, maxBin+1000)
-#     hist.Fit("GaussFit", "R")
-#     print "mean=", fit.GetParameter(1)
-#     return fit.GetParameter(1)
 
 
 if __name__ == '__main__':
@@ -116,18 +70,11 @@ if __name__ == '__main__':
     colors=[kGreen+2, kRed, kBlue, kBlack, kViolet, kMagenta]
 
 
-    directory="ThresholdScan/A06-W0110/"
-#    source="In"
+    home = environ['HOME']
+    path=home+"/eos/clicdp/data/VertexCalibration/ThresholdScan/"
+    directory=path+assembly+"/"
     path=directory+source
-#    assembly="A06-W0110"
-#    frames=5
     THLstep=2
-
-#    THLmin=20 #In
-#    THLmax=120 #In
-
-    # THLmin=20 #Zr 
-    # THLmax=328 #Zr
 
 
 
@@ -135,7 +82,7 @@ if __name__ == '__main__':
     THLs=[]
     counts=[]
 
-    txtfile = open( assembly+"_"+source+"_lxbatch.txt", "w")
+    txtfile = open( assembly+"_"+source+".txt", "w")
 
     for thl in range(THLmin, THLmax, THLstep):
         
@@ -146,116 +93,46 @@ if __name__ == '__main__':
             f = open(path+"/"+filename,'r')
             matrix = [ map(int,line.split(' ')) for line in f ]
 
-            print "Occupancy: %d pixels"%(np.count_nonzero(matrix))
+            # print "Occupancy: %d pixels"%(np.count_nonzero(matrix))
 
-            # ====== fclusterdata used for clustering ====== #
-            coords=np.array(np.transpose(np.nonzero(matrix))) # coordinate of non-zero elements
-            row=[]
-            col=[]
-            countForThisFrame=[]
+            # ============ Without clustering ============ #
+            count+=sum(sum(matrix, []))
+            # ============ End without clustering ============ #
 
-            for i in range(0, len(coords)):
-                x=coords[i][0]
-                y=coords[i][1]
-                row.append(x)
-                col.append(y)
-                countForThisFrame.append(matrix[x][y])
+            # # ====== fclusterdata used for clustering ====== #
+            # coords=np.array(np.transpose(np.nonzero(matrix))) # coordinate of non-zero elements
+            # row=[]
+            # col=[]
+            # countForThisFrame=[]
+
+            # for i in range(0, len(coords)):
+            #     x=coords[i][0]
+            #     y=coords[i][1]
+            #     row.append(x)
+            #     col.append(y)
+            #     countForThisFrame.append(matrix[x][y])
 
 
 
-            ClusteringResult=fclusterdata(coords, sqrt(2.), criterion="distance") #clustering
-            NbOfOccurance=np.bincount(ClusteringResult) # occurance of each value
-            for i in range(1, len(NbOfOccurance)):
-                if (NbOfOccurance[i]!=1):
-                    multiHitClusters=np.where(ClusteringResult==i)
-                    for j in range(0, len(multiHitClusters)):
-                        row.pop(j)
-                        col.pop(j)
-                        countForThisFrame.pop(j)
+            # ClusteringResult=fclusterdata(coords, sqrt(2.), criterion="distance") #clustering
+            # NbOfOccurance=np.bincount(ClusteringResult) # occurance of each value
+            # for i in range(1, len(NbOfOccurance)):
+            #     if (NbOfOccurance[i]!=1):
+            #         multiHitClusters=np.where(ClusteringResult==i)
+            #         for j in range(0, len(multiHitClusters)):
+            #             row.pop(j)
+            #             col.pop(j)
+            #             countForThisFrame.pop(j)
 
-            count+=sum(countForThisFrame)
-            # ============ End fclusterdata ============== #
+            # count+=sum(countForThisFrame)
+            # # ============ End fclusterdata ============== #
+
+            # # ===== using simple clustering ==== #
+            # count+=SimpleClustering(matrix)
+            # # ============ End using simple clustering ==== #
 
         counts.append(count)
         txtfile.write(str(thl)+" "+str(count)+"\n")
         print "Processing THL="+str(thl)+", Counts="+str(count)
 
     txtfile.close()
-#    writeFile(THLs, counts, assembly+"_"+source+"test.txt")
-
-"""
-    g = TGraph(len(THLs), array('d', THLs), array('d', counts))
-    g.SetMarkerStyle(20)
-    g.SetMarkerSize(0.8)
-    g.SetTitle("; THL; Counts");
-    g.Draw("AP")
-    # bla=raw_input()
-
-    print "Calculating the derivative ..."
-    deriv=derivative(THLs, counts, kRed)
-    deriv.SetTitle("; THL; #delta(Counts)/#delta(THL)");
-    print "Fitting the derivative with a Gaussian ... "
-    # fitGaus=TF1("GaussFit", "gaus", 250, 270) #Cu
-    # fitGaus=TF1("GaussFit", "gaus", 50, 80) #In
-    fitGaus=TF1("GaussFit", "gaus", 150, 190) #Zr
-    fitGaus.SetLineColor(kBlue)
-    deriv.Fit("GaussFit", "R")
-
-
-
-    canvDeriv=TCanvas("deriv", "deriv")
-    deriv.Draw("ALP")
-
-    gPad.SetGrid(1,1)
-
-    #bla=raw_input()
-"""
-
-
-
-
-
-
-
-#######################################################
-# for (path, dirnames, filenames) in os.walk(assembly_path):
-#     dataFiles=[x for x in filenames if not ".dsc" in x]
-
-#         count=0
-#         for filename in dataFiles:
-#             # THLs.append(int(filename.split("In_THL")[1]))
-#             # THLs.append(int(filename.split("A06-W0110_Fe55_THL")[1]))
-#             temp=(filename.split(source+"_THL")[1])
-#             THLs.append(int(temp.split("_")[0]))
-#             f = open(assembly_path+"/"+filename,'r')
-#             matrix = [ map(int,line.split(' ')) for line in f ]
-#             count=sum(sum(matrix, []))
-#             # count=np.count_nonzero(matrix)/(256.0*256.0)
-#             counts.append(count)
-            
-#     canvSCurve=TCanvas("canv", "canv")
-#     g = TGraph(len(THLs), array('d', THLs), array('d', counts))
-#     g.SetMarkerStyle(25)
-#     g.SetMarkerSize(0.8)
-#     g.SetMarkerColor(colors[0])
-#     g.SetLineColor(colors[0])
-    
-#     mg.Add(g)
-#     leg.AddEntry(g, "Fe55", "l")
-
-#     mg.SetTitle("A06-W0110; THL; Counts");
-#     mg.Draw("AP")
-#     leg.Draw()
-#     gPad.SetGrid(1,1)
-#     gPad.Update()   
-
- 
-
-#     # deriv=derivative(THLs, counts, kRed)
-#     # canvDeriv=TCanvas("deriv", "deriv")
-#     # deriv.Draw("AP")
-
-#     # gPad.SetGrid(1,1)
-
-#     bla=raw_input()
-
--- THLcalibration.py
+++ THLcalibration.py
@@ -7,14 +7,16 @@ import math
 import os
 import numpy as np
 
-def derivative(x, y):
+def derivative(x, y, source, assembly):
 
     x_deriv=[]
     deriv=[]
     for i in range(0, len(x)-1):
         x_deriv.append(x[i])
-        deriv.append((y[i+1]-y[i])/(x[i+1]-x[i]))
-
+        if (assembly=="A06-W0110" or assembly=="C04-W0110"):
+            deriv.append((y[i+1]-y[i])/(x[i+1]-x[i]))
+        elif (assembly=="B06-W0125"):
+            deriv.append(-(y[i+1]-y[i])/(x[i+1]-x[i]))
 
     g_deriv=TGraph(len(x_deriv), array('d', x_deriv), array('d', deriv))
     g_deriv.SetMarkerStyle(20)
@@ -22,15 +24,15 @@ def derivative(x, y):
 
 #    g_deriv.SetMarkerColor(color)
 #    g_deriv.SetLineColor(color)
-    g_deriv.SetTitle("; THL; #delta(Counts)/#delta(THL)");
+    g_deriv.SetTitle(assembly+": "+source+"; THL; #delta(Counts)/#delta(THL)");
 
     return g_deriv
 
 
-def readTextFile(filename):
-    f = open(filename)                                                                                                                                  
-    lines = [line.strip() for line in open(filename)]                                                                                                   
-    f.close()                                                                                                                                           
+def readTextFile(filename, source, assembly):
+    f = open(filename)
+    lines = [line.strip() for line in open(filename)]
+    f.close()
 
     THL=[]
     counts=[]
@@ -42,71 +44,128 @@ def readTextFile(filename):
     g = TGraph(len(THL), array('d', THL), array('d', counts))
     g.SetMarkerStyle(20)
     g.SetMarkerSize(0.8)
-    g.SetTitle("; THL; Counts");
+    g.SetTitle(assembly+": "+source+"; THL; Counts");
 
-    g_deriv=derivative(THL, counts)
+    g_deriv=derivative(THL, counts, source, assembly)
 
     return g, g_deriv
 
+def errorPropagation(x, y, fit, a, b):
+    
+    n=len(x)
+    temp=0
+    for i in range(0, n):
+        temp+=(y[i]-fit.Eval(x[i]))**2
 
+    S=TMath.Sqrt(temp/(n-2))
+    x_avg=sum(a)
+
+
+def usage():
+    print 'THL calibration'
+    print 'Usage:\n python %s <assembly> <THL to evaluate>' % ( sys.argv[0] )
 
 if __name__ == '__main__':
+
+    if (len(sys.argv)<3 or len(sys.argv)>3):
+        usage()
+        sys.exit( 1 )
+
+    assembly=sys.argv[1]
+    THL_to_evaluate=int(sys.argv[2])
+        
     gROOT.ProcessLine(".L ~/CLICdpStyle/rootstyle/CLICdpStyle.C");
     CLICdpStyle();
     gStyle.SetOptFit()
     gStyle.SetOptTitle(1);
 
-    assembly="A06-W0110"
-    sources=["In"]#, "Zr", "Cu"]
-    energies=[24.209]#, 8.048, 15.77]
+
+    sources=["Cu", "Zr", "Pd", "In"]
+    energies=[8.048, 15.77, 21.178, 24.209]
+    energies_err=[0, 0, 0, 0]
+    fitGausMin=[]
+    fitGausMax=[]
+
+    if (assembly=="A06-W0110"):
+        fitGausMin=[250, 150, 80, 50]
+        fitGausMax=[270, 190, 120, 80]
+    elif (assembly=="C04-W0110"):
+        fitGausMin=[340, 240, 180, 140]
+        fitGausMax=[360, 270, 200, 170]
+    elif (assembly=="B06-W0125"):
+        fitGausMin=[475, 565, 625, 660]
+        fitGausMax=[490, 580, 645, 690]
 
     meanTHL=[]
     meanTHLerr=[]
 
     for i in range(0, len(sources)):
-        filename=assembly+"_"+sources[i]+"_test1.txt"
+        filename=assembly+"_"+sources[i]+".txt"
+        # filename=assembly+"_"+sources[i]+"_test1.txt"
 
         # Create s-curve
-        sCurve, sCurveDeriv=readTextFile(filename)
+        sCurve, sCurveDeriv=readTextFile(filename, sources[i], assembly)
         
         # Fit with gauss
-        fitGaus=TF1("GaussFit", "gaus", 50, 80)
+        fitGaus=TF1("GaussFit", "gaus", fitGausMin[i], fitGausMax[i])
         fitGaus.SetLineColor(kBlue)
         sCurveDeriv.Fit("GaussFit", "R")
         
         # Draw the derivative
         canvDeriv=TCanvas(sources[i], sources[i])
-        sCurveDeriv.Draw("ALP")
-        
+        sCurveDeriv.Draw("ALP")        
+
+
         canvScurve=TCanvas("Scurve"+sources[i], "Scurve"+sources[i])
         sCurve.Draw("AP")
-
+        # bla=raw_input()
         # Get Mean and its error
-        meanTHL.append(fitGaus.GetParameter(0))
-        meanTHLerr.append(fitGaus.GetParError(0))
+        meanTHL.append(fitGaus.GetParameter(1))
+        meanTHLerr.append(fitGaus.GetParError(1))
 
-        print "mean=", fitGaus.GetParameter(0)
-        print "mean error=", fitGaus.GetParError(0)
+        print "mean=", fitGaus.GetParameter(1)
+        print "mean error=", fitGaus.GetParError(1)
+    
+        # bla=raw_input()
+
+
+    gr=TGraphErrors(len(energies), array('d', energies), array('d', meanTHL), array('d', energies_err), array('d', meanTHLerr))
     
 
-    bla=raw_input()
+    #gr=TGraph(len(energies), array('d', meanTHL), array('d', energies))
 
-"""    
-    gr=TGraphErrors(len(energies), array('d', meanTHL), array('d', energies), array('d', meanTHLerr), 0)
-    THL_fit=TF1("THL","[0]*x+[1]")
+    THL_fit=TF1("THL","[0]*x+[1]", 0, 300)
+
     THL_fit.SetParName(0,"a")
     THL_fit.SetParName(1,"b")
-    gr.SetMarkerStyle(8)
+    gr.SetMarkerStyle(25)
+    gr.SetMarkerSize(0.8)
+    THL_fit.SetLineColor(kGreen+2)
     gr.SetLineColor(kBlack)
     gr.SetTitle(assembly)
-    gr.GetXaxis().SetTitle("THL")
-    gr.GetYaxis().SetTitle("Energy [keV]")
+    gr.GetXaxis().SetTitle("Energy [keV]")
+    gr.GetYaxis().SetTitle("THL")
 
-    gr.Fit("THL")
+    fit_result=gr.Fit("THL", "S")
     gr.Draw("AP")
     gPad.Update()
     gStyle.SetOptFit() 
-    
-    print "THL 326 corresponds to:", THL_fit.Eval(326)
-"""
 
+    a=THL_fit.GetParameter(0)
+    b=THL_fit.GetParameter(1)
+    print "a=", a
+    print "b=", b
+    
+    covMat=fit_result.GetCovarianceMatrix()
+    sigma_a=covMat[0][0]
+    sigma_ab=covMat[0][1]
+    sigma_b=covMat[1][1]
+
+    y_eval=THL_to_evaluate
+    x_eval=THL_fit.GetX(y_eval)
+    
+    err_x=TMath.Sqrt(((y_eval-b)**2)/(a**4)*sigma_a+sigma_b/(a**2)+2*sigma_ab*(y_eval-b)/(a**3))
+
+    print "THL %d corresponds to: %f [keV] +- %f [keV]"%(y_eval, x_eval, err_x)
+
+    bla=raw_input()
