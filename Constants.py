728a1bfc7e524dab435b4025bf64b6ec4b058ab5
Sophie Redford <sophie.redford@gmail.com>
2015-05-05 14:27:41 +0200

    Added LNLS data to plotStats.py
    In Constants.py:
    - added LNLS sources to known sources
    - also defined CERN sources and LNLS sources
    
    In plotStats.py
    - check LNLS data is only requested for A06-W0110
    - load relevant LNLS data
    - also added a maxStat parameter, allowing a variable
    upper limit in the statistics histogram

--- Constants.py
+++ Constants.py
@@ -6,8 +6,11 @@ npixY = 256
 
 # known inputs
 known_assemblies = ["A06-W0110","B06-W0125","B07-W0125","C04-W0110","D09-W0126","L04-W0125"]
-known_sources = ["Fe","Co","Cd","CuIn","Am"]
+known_sources = ["Fe","Co","Cd","CuInXRF","Am","CoXRF","CrXRF","CuXRF","FeXRF","MnXRF","NiXRF","TiXRF","VXRF"]
 known_peaks = ["Fe","Co1","Cu","Co2","Cd","In","Am2","Am3"]
+
+CERN_sources = ["Fe","Co","Cd","CuInXRF","Am"]
+LNLS_sources = ["CoXRF","CrXRF","CuXRF","FeXRF","MnXRF","NiXRF","TiXRF","VXRF"]
 
 # peak energies in keV
 FePeakE = 5.899
