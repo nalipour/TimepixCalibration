fb642a308c455ebc9579964959c2232b2dfb5629
Sophie Redford <sophie.redford@gmail.com>
2015-05-06 11:22:57 +0200

    Added LNLS data into global peak finding.
    In Constants.py:
    - added LNLS peaks to known peaks
    - also created CERN_peaks and LNLS_peaks
    - be careful - CuXRF data recorded at CERN and LNLS!
    
    In calibrateKDEGlobal.py:
    - load LNLS data
    - check LNLS data only with A06-W0110
    - decided to put plots into a global directory, to stop them being
    mixed up with all the pixel plots

--- Constants.py
+++ Constants.py
@@ -7,10 +7,13 @@ npixY = 256
 # known inputs
 known_assemblies = ["A06-W0110","B06-W0125","B07-W0125","C04-W0110","D09-W0126","L04-W0125"]
 known_sources = ["Fe","Co","Cd","CuInXRF","Am","CoXRF","CrXRF","CuXRF","FeXRF","MnXRF","NiXRF","TiXRF","VXRF"]
-known_peaks = ["Fe","Co1","Cu","Co2","Cd","In","Am2","Am3"]
+known_peaks = ["Fe","Co1","CuXRF_CERN","Co2","Cd","InXRF","Am2","Am3","CoXRF","CrXRF","CuXRF_LNLS","FeXRF","MnXRF","NiXRF","TiXRF","VXRF"]
 
 CERN_sources = ["Fe","Co","Cd","CuInXRF","Am"]
 LNLS_sources = ["CoXRF","CrXRF","CuXRF","FeXRF","MnXRF","NiXRF","TiXRF","VXRF"]
+
+CERN_peaks = ["Fe","Co1","CuXRF_CERN","Co2","Cd","InXRF","Am2","Am3"]
+LNLS_peaks = ["CoXRF","CrXRF","CuXRF_LNLS","FeXRF","MnXRF","NiXRF","TiXRF","VXRF"]
 
 # peak energies in keV
 FePeakE = 5.899
